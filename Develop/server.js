80d416f82e754306d0410a9d8041b6bf3d9bd07c
Jake Wagner <jkwagneriii@gmail.com>
2020-10-24 13:35:21 -0500

    Standard server.js set up referenced from class activities

---
+++ Develop/server.js
@@ -0,0 +1,6 @@
+//Standard server.js set up referenced from class activities
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
