980b1d8872b1743a8cd1c32fc17b5f9cd4e90d0f
Jake Wagner <jkwagneriii@gmail.com>
2020-10-24 14:07:46 -0500

    index html page is loading on the localhost:

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,38 @@
-//Standard server.js set up referenced from class activities
+// Dependencies
+// =============================================================
 const express = require('express');
 const path = require('path');
 
+// Sets up the Express App
+// =============================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+// =============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sets up the Express app to work properly with static files
+// =============================================================
+app.use(express.static('public'));
+
+// Routes
+// =============================================================
+
+//Root 
+app.get("/", function(req, res) {
+    console.log('I AM ROOT')
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT http://localhost:" + PORT);
+});
+
